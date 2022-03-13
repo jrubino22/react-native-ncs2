36a637baeaf20c84596b9eaf73f60fb0c4faa6c4
Joseph Rubino <joeyrubi22@gmail.com>
2022-03-12 16:17:28 -0800

    info and reservation components updated for alert and gestures

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -29,6 +29,8 @@ function RenderCampsite(props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true: false;
 
+    const recognizeComment = ({dx}) => (dx > 200) ? true: false;
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -56,7 +58,12 @@ function RenderCampsite(props) {
                     {cancelable: false}
                 )
             }
-            return true;
+            else if(recognizeComment(gestureState)) {
+                props.onShowModal()
+            }
+            return true
+
+            
         }
     })
 
