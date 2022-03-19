9570b46424125560237373641bd65301c9bb90a2
Joseph Rubino <joeyrubi22@gmail.com>
2022-03-19 11:27:32 -0700

    Get image from gallery

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -175,6 +175,19 @@ class RegisterTab extends Component {
         this.setState({imageUrl: processedImage.uri});
     }
 
+    getImageFromGallery = async () => {
+        const cameraRollPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if (cameraRollPermissions.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1,1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processedImage(capturedImage.uri);
+            }
+        };
+    }
 
     handleRegister() {
         console.log(JSON.stringify(this.state));
@@ -201,6 +214,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title="Gallery"
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
