b295ddefa34b347f74611388f5480acd52ee3b78
Joseph Rubino <joeyrubi22@gmail.com>
2022-03-19 11:04:04 -0700

    Image manipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 
 class LoginTab extends Component {
@@ -153,10 +154,27 @@ class RegisterTab extends Component {
             })
             if(!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri})
+                this.processedImage(capturedImage.uri);
             }
         }
     }
+
+    processedImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [
+                {
+                    resize: {width: 400}
+                }
+            ],
+            {
+                format: ImageManipulator.SaveFormat.PNG
+            }
+            );
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri});
+    }
+
 
     handleRegister() {
         console.log(JSON.stringify(this.state));
